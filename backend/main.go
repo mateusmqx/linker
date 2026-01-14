5ce8908611c8082083ba71ffff89f4e888a37b52
Mateus Marques <mateus.mqx@gmail.com>
2026-01-14 07:57:10 -0300

    feat: read content

--- backend/main.go
+++ backend/main.go
@@ -35,5 +35,32 @@ func main() {
 	fmt.Println("--- Repositórios do usuário autenticado (Autenticado) ---")
 	for _, repo := range authenticatedUserRepos {
 		fmt.Printf("- %s\n", *repo.Name)
+		owner := repo.GetOwner().GetLogin()
+		repo := repo.GetName()
+		path := "linker-info.yml"
+		fileContent, directoryContent, _, err := clientWithAuth.Repositories.GetContents(ctx, owner, repo, path, nil)
+		if err != nil {
+			log.Printf("Erro ao obter conteúdo do linker-info.yml: %v", err)
+			continue
+		}
+
+		// 2. Verificação do tipo de conteúdo retornado
+		if directoryContent != nil {
+			log.Printf("Esperava um arquivo, mas o caminho especificado é um diretório contendo %d itens.", len(directoryContent))
+		}
+
+		// Verifica se é realmente um arquivo e não um diretório
+		if fileContent == nil {
+			log.Printf("O caminho especificado é um diretório, não um arquivo.")
+		}
+
+		// 3. Decodificação do conteúdo
+		// O conteúdo vem em Base64, mas a lib tem este helper que decodifica para string
+		content, err := fileContent.GetContent()
+		if err != nil {
+			log.Printf("Erro ao decodificar conteúdo: %v", err)
+		}
+
+		fmt.Printf("Conteúdo do arquivo:\n%s\n", content)
 	}
 }
