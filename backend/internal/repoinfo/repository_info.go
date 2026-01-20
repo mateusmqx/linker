5647a5644c9758d54b7f4ee05e2a14ef622d140b
Mateus Marques <mateus.mqx@gmail.com>
2026-01-20 10:01:56 -0300

    chore: clean repo

--- backend/internal/repoinfo/repository_info.go
+++
@@ -1,8 +0,0 @@
-package repoinfo
-
-type RepositoryInfo struct {
-	Owner        string   `yaml:"owner"`
-	Name         string   `yaml:"name"`
-	Domain       string   `yaml:"domain"`
-	Dependencies []string `yaml:"dependencies"`
-}
